055f9ec084bc57aa992f010674c429fd23980d69
crdunwel <clayton.dunwell@gmail.com>
2018-07-01 16:28:06 -0400

    solution for set solver and test cases

---
+++ setsolver.py
@@ -0,0 +1,129 @@
+import itertools
+import random
+import json
+import argparse
+
+
+class Card(object):
+
+	def __init__(self, idval, dimensions=None):
+		self.id = idval
+		self.dims = dimensions or {}
+
+	def to_dict(self):
+		return {'id': self.id, 'dims': self.dims}
+
+
+class SetGame(object):
+
+	class InvalidCardError(Exception):
+
+		def __init__(self, e):
+			msg = 'Invalid deck. Card "{0}" contains ' \
+			'invalid data "{1}"'.format(e[0], e[1])
+			super().__init__(msg)
+
+	def __init__(self, dim_map=None, cards=None):
+		self.cards = cards or []
+		self.dim_map = dim_map or {}
+
+	def addCard(self, card):
+		self.cards.append(card)
+
+	def make_deck(self):
+		self.cards = []
+		attrs = [v for k,v in self.dim_map.items()]
+		i = 0
+		for element in itertools.product(*attrs):
+			dims = dict(zip(self.dim_map.keys(), element))
+			self.addCard(Card(i, dims))
+			i+=1
+
+	def load_dims_from_file(self, filename='dims.json'):
+		try:
+			with open(filename, 'r') as f:
+				self.dim_map = json.load(f)
+		except FileNotFoundError:
+			print('{0} not found.'.format(filename))
+		except ValueError:
+			print('{0} is not a valid json file.'.format(filename))
+
+	def write_deck_to_file(self, filename="deck.json"):
+		with open(filename, 'w+') as f:
+			toWrite = {
+				'dimensions': self.dim_map,
+				'cards': [c.to_dict() for c in self.cards]
+			}
+			f.write(json.dumps(toWrite, indent=2))
+
+	def valid_deck(self, deck):
+		for c in deck['cards']:
+			for d,v in c['dims'].items():
+				if d not in deck['dimensions'].keys():
+					return (c['id'], d)
+				if v not in deck['dimensions'][d]:
+					return (c['id'], v)
+		return True
+
+	def load_deck_from_file(self, filename="deck.json"):
+		self.cards = []
+		with open(filename, 'r') as f:
+			deck_data = json.load(f)
+
+			vd = self.valid_deck(deck_data)
+			if vd != True:
+				raise self.InvalidCardError(vd)
+
+			self.dim_map = deck_data['dimensions']
+			for c in deck_data['cards']:
+				self.addCard(Card(c['id'], c['dims']))
+
+	def valid_set(self, card_combo):
+		for prop in self.dim_map.keys():
+			set_length = len(set([c.dims[prop] for c in card_combo]))
+			if set_length != 1 and set_length != len(card_combo):
+				return False
+		return True
+
+	def finish_set(self, uset):
+		sets = []
+		for c in self.cards:
+			if c.id in [u.id for u in uset]:
+				continue
+			elif self.valid_set(uset + (c,)):
+				sets.append(c.id)
+		return sets
+
+	def possible_sets(self, choose=3):
+	  sets = []
+	  combos = itertools.combinations(self.cards, choose)
+	  for combo in combos:
+	  	if self.valid_set(combo):
+	  		sets.append(tuple(c.id for c in combo))
+	  return sets
+	
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--dims', type=str,
+  	help='dimension json file with possible attributes and values')
+	parser.add_argument('--deck', type=str,
+    help='deck json file with cards to play')
+	parser.add_argument('--choose', nargs='?', default=3, type=int,
+    help='number of cards required for a set')
+
+	args = parser.parse_args()
+
+	sg = SetGame()
+	if args.deck:
+		sg.load_deck_from_file(args.deck)
+	elif args.dims:
+		sg.load_dims_from_file(args.dims)
+		sg.make_deck()
+	else:
+		raise Exception('Must input dimensional map or deck file.')
+
+	print(sg.possible_sets(args.choose))
+
+if __name__ == "__main__":
+    main()
---
+++ tests.py
@@ -0,0 +1,93 @@
+from setsolver import SetGame, Card
+import unittest
+import os
+import itertools
+import timeit
+
+class TestSetGame(unittest.TestCase):
+
+	TEST_DATA_DIR = 'test_data/'
+
+	def file_path(self, file):
+		return os.path.join(self.TEST_DATA_DIR, file)
+
+	def setUp(self):
+		self.sg = SetGame()
+
+	def test_invalid_deck(self):
+		self.assertRaises(SetGame.InvalidCardError,
+											self.sg.load_deck_from_file,
+											self.file_path('decks/invalid_deck.json'))
+
+	def test_valid_deck(self):
+		self.sg.load_deck_from_file(self.file_path('decks/valid_deck.json'))
+
+	def test_set_solver(self):
+		self.sg.load_deck_from_file(self.file_path('decks/set_solver1.json'))
+		sets = self.sg.possible_sets()
+		assert(sets == [(0, 1, 2), (1, 2, 3)])
+
+	def test_finish_set(self):
+		self.sg.load_deck_from_file(self.file_path('decks/set_solver1.json'))
+		finish = self.sg.finish_set((self.sg.cards[0], self.sg.cards[1]))
+		assert(finish == [2])
+		finish = self.sg.finish_set((self.sg.cards[1], self.sg.cards[2]))
+		assert(finish == [0,3])
+		finish = self.sg.finish_set((self.sg.cards[3], self.sg.cards[1]))
+		assert(finish == [2])
+
+	def test_all_possible_combos(self):
+		"""
+		Generates all possible cards for a standard set deck then uses the
+		set solver to determine if the correct number of sets exist.
+
+		There are 4 dimensions with 3 possible values so there are 3^4 = 81 cards
+		in a deck. To find the number of possible sets we must first notice that
+		for any two cards in a deck there is only one possible card that will
+		create a valid set. Thus if two cards have the same value for an attribute
+		then the third card must also have that value. If the two cards have
+		different values for an attribute then the third card must have a
+		different value from the other two. For a standard set deck as used in
+		this test case with 81 cards and 4 dimensions with 3 possible values, this
+		would yield (81*80)/3! = 1080 possible sets. Dividing by 3! is necessary
+		because there are 3!=6 ways to order three items.
+
+		Since the number of possible sets can be defined as (n choose k) where n
+		is the size of the deck (possible combinations) and k is the set size then
+		set solver is O(n^k) as choose formula is n! / (k!(n-k)!) which is a
+		kth-degree polynomial.
+
+		tl;dr
+
+		example 1:
+		3^4 = 81 cards
+		81 choose 3 = 85320 possible sets
+		(81*80*1)/3! = 1080 valid sets
+
+		example 2
+		3^5 = 243 cards
+		243 choose 3 = 2362041 possible sets
+		(243*242*1)/3! = 9801 valid sets
+		"""
+
+		timer_string = "{0} seconds to solve for {1} cards with {2} dimensions " \
+									 "containing {3} possible values"
+
+		dim_maps = [
+			('dims/simple_dim1.json', 3, 1080),
+			('dims/simple_dim2.json', 3, 9801),
+		]
+
+		for dim in dim_maps:
+			self.sg.load_dims_from_file(self.file_path(dim[0]))
+			self.sg.make_deck()
+			start_time = timeit.default_timer()
+			assert(len( self.sg.possible_sets(dim[1])) == dim[2])
+			elapsed = timeit.default_timer() - start_time
+			print(timer_string.format(elapsed,
+															 	len(self.sg.cards),
+															 	len(self.sg.dim_map.keys()),
+															 	len(list(self.sg.dim_map.values())[0])))
+
+if __name__ == '__main__':
+    unittest.main()
